f89c270133e246464f9b27f162d6765b2969878f
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-20 17:54:56 +0200

    il timer non worka (NaN)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = createApp({
     data (){
 
         return{
-                    //dichiarazione immagini
+                    //array immagini
 
             images :[
                 'imgs/01.webp',
@@ -14,6 +14,8 @@ const app = createApp({
                 'imgs/04.webp',
                 'imgs/05.webp',
             ],
+                   
+                    //array titoli
 
             titles:[
 
@@ -23,6 +25,8 @@ const app = createApp({
                 'Stray',
                 "Marvel's Avengers",
             ],
+                  
+                    //array descrizioni
                
             text:[
 
@@ -32,9 +36,10 @@ const app = createApp({
                 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
             ],
+
             activeIndex : 0,
-            
-            
+            countDown: 3000,
+
         }
 
     },
@@ -59,30 +64,16 @@ const app = createApp({
                  this.activeIndex = 0;
             }
         }
-    }
+    },
+    mounted(){
+    
+        function incrementa() {
+            this.activeIndex = parseInt(this.activeIndex + 1);
+            console.log(this.activeIndex);
+          }
+          
+          setInterval(incrementa, this.countDown);
+    },
 
 }).mount('#app')
 
-/*const cards = [
-    {
-        image: 'imgs/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'imgs/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'imgs/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'imgs/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'imgs/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-]*/
